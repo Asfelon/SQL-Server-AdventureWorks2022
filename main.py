b656eb86cfad3a6816506c4812b33590eccf8231
Anuj Dalmia <anujdalmia98@gmail.com>
2024-08-31 10:49:54 +0530

    Standardized

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from connector import Connector
 
 
-connector = Connector()
-connection = connector.connection
-cursor = connector.get_cursor()
+if "__name__" == "__main__":
+    connector = Connector()
+    connection = connector.connection
+    cursor = connector.get_cursor()
 
-recovery_database_name = "AdventureWorks2022"
-# connector.recover_database(recovery_database_name)
+    recovery_database_name = "AdventureWorks2022"
+    # connector.recover_database(recovery_database_name)
 
-connector.tables_to_csv(connector.get_all_tables())
+    connector.tables_to_csv(connector.get_all_tables())
